18398436cce96159b7de1a7b70915c23b6fd5030
Weslley Hinsch <wes.hinsch@gmail.com>
2024-07-29 14:27:25 -0300

    correcoes

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -1,14 +1,9 @@
 "use client"
 
-import { useSession } from "next-auth/react"
-
 export default function AboutPage() {
-  const { data } = useSession()
-
   return (
     <div className="w-full max-w-screen-xl h-screen flex justify-center items-center">
       <h1>Sobre page</h1>
-      {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
     </div>
   )
 }
