fdf5a296b8d0d83567e444fb90099f4e08394e8b
Weslley Hinsch <wes.hinsch@gmail.com>
2024-07-29 14:24:51 -0300

    correcoes

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -3,12 +3,12 @@
 import { useSession } from "next-auth/react"
 
 export default function AboutPage() {
-  const { data: session } = useSession()
+  const { data } = useSession()
 
   return (
     <div className="w-full max-w-screen-xl h-screen flex justify-center items-center">
       <h1>Sobre page</h1>
-      {session && <pre>{JSON.stringify(session, null, 2)}</pre>}
+      {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
     </div>
   )
 }
